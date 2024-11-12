26345a657ae139fa83050a21285b64a974be89a1
Sala K 406 <salak406@salasfr.itm.edu.co>
2024-11-12 07:38:56 -0500

    hola

---
+++ Inventario/InventarioControlador/Entidades/Tipo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioControlador.Entidades
+{
+    public  class Tipo
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Tipo")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")]
+        [Required(ErrorMessage = "el campo {0} es obligatorio")]
+        public string Name { get; set; } = null!;
+        public string ? Categoria { get; set; }
+    }
+}
